c1431f0a5ccc0e6542b450170348dc119729df4f
bidipeppercrap <bidipeppercrap@outlook.com>
2021-10-01 22:26:42 +0700

    Initialization with Contacts in mind

---
+++ pages/contacts.js
@@ -0,0 +1,28 @@
+import { supabase } from '@/utils/supabase-client'
+
+export default function Contacts({ contacts }) {
+    return (
+        <div className="page__layout">
+            <h1 className="page__title">Contacts</h1>
+            <ul className="contact__list">
+                {contacts.map((contact) => (
+                    <li key={contact.id} className="contact__item">
+                        {contact.title}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export async function getStaticProps() {
+    const { data, error } = await supabase.from('contacts').select(`
+        id, title, url, icon_url
+    `)
+
+    return {
+        props: {
+            contacts: data
+        }
+    }
+}
